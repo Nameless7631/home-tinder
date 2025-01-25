ae599360cd5781341367c0904f79d9d6a9a7b892
Nathan Nguyen <nathnguyen7631@gmail.com>
2025-01-24 21:39:02 -0800

    repository folder

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import { Input } from "@chakra-ui/react"
+import { Swipe } from "./src/components/swipe.jsx"
+
+function App() {
+  return (
+    <>
+    
+    </>
+  );
+}
+
+export default App;
