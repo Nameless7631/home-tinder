583d5d7a3a96979802dcef5deee62de5ed0585ab
ejleee <ej1026115@gmail.com>
2025-01-25 22:58:38 -0800

    Co-authored-by: brelieu05 <brelieu05@users.noreply.github.com>

--- src/components/ui/description.jsx
+++ src/components/ui/description.jsx
@@ -1,46 +1,47 @@
 import { useState } from "react";
 import { Box } from "@chakra-ui/react";
 
-
-const Description = ({address, bed, bath, sqft,text}) => {
+const Description = ({ address, bed, bath, sqft, text }) => {
     const [isOpen, setIsOpen] = useState(false);
     const handleToggle = () => {
         setIsOpen(prevState => !prevState);
     };
-    const halfwayIdx = (text.length > 120) ? Math.floor(text.length/2) : text.length;
+
+    const halfwayIdx = (text.length > 120) ? Math.floor(text.length / 2) : text.length;
     const firstHalf = text.slice(0, halfwayIdx);
     const secondHalf = text.slice(halfwayIdx);
 
     return (
-    <Box>
-        <Box onClick={handleToggle} style={{ cursor: "pointer", display: "inline"}}>
-            {(address) ? (
-                <>
-                Address: {address}<br/>
-                </>
-            ) : <></>}
-             {(bed > 0) ? (
-                <>
-                Bed: {bed}<br/>
-                </>
-            ) : <></>}
-             {(bath > 0) ? (
-                <>
-                Bath: {bath}<br/>
-                </>
-            ) : <></>}
-            {(sqft > 0) ? (
-                <>
-                Sqft: {sqft}<br/>
-                </>
-            ) : <></>}
-            <br/>
-            {firstHalf}
-        </Box>
-        {isOpen && (
-            <Box style={{ display: "inline"}}>
-                {secondHalf}
+        <Box>
+            <Box onClick={handleToggle} style={{ cursor: "pointer", display: "inline" }}>
+                {address && (
+                    <>
+                        Address: {address}<br />
+                    </>
+                )}
+                {bed > 0 && (
+                    <>
+                        Bed: {bed}<br />
+                    </>
+                )}
+                {bath > 0 && (
+                    <>
+                        Bath: {bath}<br />
+                    </>
+                )}
+                {sqft > 0 && (
+                    <>
+                        Sqft: {sqft}<br />
+                    </>
+                )}
+                <br />
+                {firstHalf}
             </Box>
+            {isOpen && (
+                <Box style={{ display: "inline" }}>
+                    {secondHalf}
+                </Box>
+            )}
             {isOpen && (
                 <Box style={{ display: "inline", marginLeft: "5px" }}>
                     text ever since the 1500s, when an unknown printer took a
@@ -49,9 +50,6 @@ const Description = ({address, bed, bath, sqft,text}) => {
             )}
         </Box>
     );
-        )}
-    </Box>
-  );
 };
 
 export { Description };
