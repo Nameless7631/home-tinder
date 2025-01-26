3148fee35096a7e399a8491738a7da0fd7a0debd
Nathan Nguyen <nathnguyen7631@gmail.com>
2025-01-26 01:41:27 -0800

    fix description

--- src/components/ui/description.jsx
+++ src/components/ui/description.jsx
@@ -42,14 +42,9 @@ const Description = ({ address, bed, bath, sqft, text }) => {
         {isOpen && (
             <Box style={{ display: "inline"}}>
                 {secondHalf}
-                <Box style={{ display: "inline" }}>
-                    text ever since the 1500s, when an unknown printer took a
-                    galley of type and scrambled it to make a type specimen book.
-                </Box>
             </Box>
         )}
-
-      </Box>
+    </Box>
   );
 };
 
